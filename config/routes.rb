144948c4b7f2edf76966246be6c1e4a302de3e27
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-04-01 17:57:31 -0500

    Added many to many migration for the users and events model along with the users, events and static pages controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'events/index'
+  get 'events/new'
+  get 'events/edit'
+  get 'events/create'
+  get 'events/update'
+  get 'events/destroy'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_01_224245) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "location"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events_users", id: false, force: :cascade do |t|
+    t.bigint "creator_id"
+    t.bigint "attendant_id"
+    t.bigint "event_id"
+    t.index ["attendant_id"], name: "index_events_users_on_attendant_id"
+    t.index ["creator_id"], name: "index_events_users_on_creator_id"
+    t.index ["event_id"], name: "index_events_users_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
